c80af08ffde3e93935da550a6793d76ae86d4ee6
Arturo-Blanco <arturoblanco@tdf.edu.ar>
2023-11-02 13:23:41 -0300

    attributes features are added

--- src/pets/attributes/attributes.controller.ts
+++ src/pets/attributes/attributes.controller.ts
@@ -1,15 +1,36 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { AttributesService } from './attributes.service';
 import { AttributeDTO } from "./dto/attribute.dto";
+import { Attribute } from "./entities/attribute.entity";
 
 @Controller('attribute')
 export class AttributesController {
     constructor(private readonly attributesService: AttributesService) { }
 
-    @Post('addAttribute')
+    @Post('add')
+    @UsePipes(ValidationPipe)
     async getAddAttribut(@Body() attributeDTO: AttributeDTO): Promise<string> {
-        return this.attributesService.addAttribute(attributeDTO);
+        return await this.attributesService.addAttribute(attributeDTO);
     }
 
+    @Get('all')
+    async getAttributes(): Promise<Attribute[] | string> {
+        return await this.attributesService.allAttributes();
+    }
 
-}
+    @Get(':attributeId')
+    async getAttributeById(@Param('attributeId', ParseIntPipe) attributeId: number): Promise<Attribute> {
+        return await this.attributesService.getById(attributeId);
+    }
+
+    @Patch('update/:id')
+    async getUpdateAttribute(@Param('id', ParseIntPipe) attributeId: number, @Body() updateAttributeDTO: AttributeDTO): Promise<string> {
+        return await this.attributesService.updateAttribute(attributeId, updateAttributeDTO);
+    }
+
+    @Delete('delete/:attributeId')
+    async getDeleteAttribute(@Param('attributeId', ParseIntPipe) attributeId: number): Promise<string> {
+        return await this.attributesService.deleteAttribute(attributeId);
+    }
+
+}   
--- src/pets/attributes/dto/attribute.dto.ts
+++ src/pets/attributes/dto/attribute.dto.ts
@@ -1,3 +1,6 @@
+import { IsNotEmpty } from "class-validator";
+
 export class AttributeDTO {
+    @IsNotEmpty()
     readonly name : string;
 }
