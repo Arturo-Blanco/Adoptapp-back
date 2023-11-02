c6177fa0507e1c3601771b28f31a9b6af290e6d8
Arturo-Blanco <arturoblanco@tdf.edu.ar>
2023-11-02 13:24:13 -0300

    added features

--- src/pets/pets.module.ts
+++ src/pets/pets.module.ts
@@ -4,13 +4,14 @@ import { PetsController } from './pets.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pet } from './entities/pet.entity';
 import { City } from 'src/city/entities/city.entity';
-import { User } from 'src/users/entities/user.entity';
+import { Client } from 'src/clients/entities/client.entity';
 import { Attribute } from './attributes/entities/attribute.entity';
-import { Adoption } from 'src/Services/adoptions/entities/adoptions.entity';
+import { Adoption } from 'src/adoptions/entities/adoptions.entity';
+import { Institution } from 'src/institutions/entities/institution.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Pet, City, User, Attribute, Adoption])
+    TypeOrmModule.forFeature([Pet, City, Institution, Client, Attribute, Adoption])
   ],
   controllers: [PetsController],
   providers: [PetsService],
