f16ff1b2f38b032efc6ee9c6c179c45616b71719
Arturo-Blanco <arturoblanco@tdf.edu.ar>
2023-11-17 14:20:24 -0300

    cors is added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule , {cors : true});
-  app.useGlobalPipes(new ValidationPipe())
+  const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: "http://localhost:5173",
+    methods: 'GET, PUT, HEAD, POST, PATCH, DELETE',
+    credentials: true
+  });
   await app.listen(3001);
 }
 bootstrap();
