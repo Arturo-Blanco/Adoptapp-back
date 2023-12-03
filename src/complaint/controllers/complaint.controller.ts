4705c6bf89b4a85db6492e8557f52be6fe9d985c
= <dariocabrera@tdf.edu.ar>
2023-12-03 20:20:29 -0300

    crear complaint funcional

--- src/complaint/controllers/complaint.controller.ts
+++ src/complaint/controllers/complaint.controller.ts
@@ -6,7 +6,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { v4 as uuid4 } from 'uuid';
 import { CreateComplaintDTO } from '../dto/complaint.dto';
 
-@Controller('complaint')
+@Controller('complaints')
 export class ComplaintController {
   constructor(
     private readonly complaintService: ComplaintService,
@@ -18,13 +18,19 @@ export class ComplaintController {
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File, @Body() body: CreateComplaintDTO): Promise<void> {
     try {
+      console.log(body);
       const newFileName = `${uuid4()}.jpeg`; // File name is change to a unique id
       const processImage = await this.imageService.processImage(file.buffer); // File is process
       const imgUrl = await this.firebaseService.uploadFile(newFileName, processImage); // File is uploaded to Firebase and return the URL
       await this.complaintService.createComplaint(imgUrl, body);
     } catch (error) {
-      throw new Error('Error processing image.');
+      throw new Error('Error processing image.' + error.message);
         }
+  }
+
+  @Get('all')
+  findAll() {
+    return this.complaintService.findAll();
   }
 
   @Get(':id')
